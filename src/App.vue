c3aea0b4d5f42bb1ffdb5f954a4fa585478bfa77
tom2991 <okhdsa.5564@gmail.com>
2021-04-06 17:10:52 +0900

    Project's Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-ons-page>
+      <v-ons-toolbar>
+        <div class="center">{{ title }}</div>
+        <div class="right">
+          <v-ons-toolbar-button>
+            <v-ons-icon icon="ion-navicon, material: md-menu"></v-ons-icon>
+          </v-ons-toolbar-button>
+        </div>
+      </v-ons-toolbar>
+      <div style="text-align: center; padding-top:10px">Hello World!</div>
+      <p style="text-align: center">
+        <v-ons-button @click="alert">Click Me!</v-ons-button>
+      </p>
+    </v-ons-page>
+</template>
+<script>
+  export default{
+    data() {
+      return {
+        title: 'My app'
+      };
+    },
+    methods: {
+      alert() {
+        this.$ons.notification.alert('This is an Onsen UI alert notification test.');
+      }
+    }
+  };
+</script>
